e9fa25665bb7827934b0d2ea69de740c7d14855f
Mark79 <mark_publicover@hotmail.com>
2014-06-07 16:38:36 -0400

    done

--- plot4.R
+++ plot4.R
@@ -15,7 +15,7 @@ png("plot4.png")
   plot(shpc$DateTime,shpc$Sub_metering_1 ,type="l",xlab="",ylab="Energy sub metering")
   points(shpc$DateTime,shpc$Sub_metering_2,type="l",col="red")
   points(shpc$DateTime,shpc$Sub_metering_3,type="l",col="blue")
-  legend(x="topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=c(1, 1, 1),lwd=c(2.5, 2.5, 2.5),col=c("black", "red", "blue"))
+  legend(x="topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),border=NULL,lty=c(1, 1, 1),bty="n",col=c("black", "red", "blue"))
 
   plot(shpc$DateTime,shpc$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
 
